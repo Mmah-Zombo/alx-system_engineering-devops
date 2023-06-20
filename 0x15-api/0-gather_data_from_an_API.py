da1ef4420d3974766a437ca924c27055e6b8748e
M'mah Zombo <www.mariamzombo@gmail.com>
2023-06-20 12:37:09 +0000

    now using mmy code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"returns information about his/her TODO list progress"
 import requests
 import sys
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    employee = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos" ,\
+            params={'userId': sys.argv[1]}).json()
+    tasks = len(todos)
+    completed_tasks = [x.get('title') for x in todos if x.get('completed') is True]
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    print("Employee {} is done with tasks({}/{}):"\
+        .format(employee['name'], len(completed_tasks), tasks))
+
+    [print("\t {}".format(each)) for each in completed_tasks]
