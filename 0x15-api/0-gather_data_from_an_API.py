0dc891f32ae45375234ecb1fc33555e353cdebb3
M'mah Zombo <www.mariamzombo@gmail.com>
2023-06-20 11:22:45 +0000

    fixed the pycodestyle validation error

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 "returns information about his/her TODO list progress"
-import requests 
+import requests
 import sys
 
 if __name__ == '__main__':
-  url = "https://jsonplaceholder.typicode.com/"
-  employee = requests.get(url + "users/{}".format(sys.argv[1])).json()
-  todos = requests.get(url + "todos", params={'userId': employee['id'],}).json()
-  tasks = len(todos)
-  completed_tasks = [x['title'] for x in todos if x['completed'] == True]
+    url = "https://jsonplaceholder.typicode.com/"
+    employee = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos" , params={'userId': employee['id'],}).json()
+    tasks = len(todos)
+    completed_tasks = [x['title'] for x in todos if x['completed'] is True]
 
-  print("Employee {} is done with tasks({}/{}):".format(employee['name'], len(completed_tasks), tasks))
+    print("Employee {} is done with tasks({}/{}):"\
+        .format(employee['name'], len(completed_tasks), tasks))
 
-  for each in completed_tasks:
-    print("\t {}".format(each))
+    for each in completed_tasks:
+        print("\t {}".format(each))
