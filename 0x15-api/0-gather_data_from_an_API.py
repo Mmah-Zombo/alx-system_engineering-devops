bb4fd867b6057a3f088a4bccc9df73bfedfd6fd7
M'mah Zombo <www.mariamzombo@gmail.com>
2023-06-20 14:31:39 +0000

    I entered the answer frpm chatgpt

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,15 +3,35 @@
 import requests
 import sys
 
-if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com/"
-    employee = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos" ,\
-            params={'userId': sys.argv[1]}).json()
-    tasks = len(todos)
-    completed_tasks = [x.get('title') for x in todos if x.get('completed') is True]
+def get_employee_todo_progress(employee_id):
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Failed to retrieve employee information for ID {employee_id}")
+        return
 
-    print("Employee {} is done with tasks({}/{}):"\
-        .format(employee['name'], len(completed_tasks), tasks))
+    employee = response.json()
+    employee_name = employee.get("name")
 
-    [print("\t {}".format(each)) for each in completed_tasks]
+    url = "https://jsonplaceholder.typicode.com/todos"
+    response = requests.get(url, params={"userId": employee_id})
+    if response.status_code != 200:
+        print(f"Failed to retrieve TODO list for employee ID {employee_id}")
+        return
+
+    todos = response.json()
+    completed_tasks = [todo for todo in todos if todo.get("completed")]
+
+    total_tasks = len(todos)
+    completed_count = len(completed_tasks)
+
+    print(f"Employee {employee_name} is done with tasks ({completed_count}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t{task.get('title')}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Please provide the employee ID as a parameter.")
+    else:
+        employee_id = sys.argv[1]
+        get_employee_todo_progress(employee_id)
