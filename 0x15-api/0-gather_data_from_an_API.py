19636a151fd439653cd92891c0b919d978d2f613
M'mah Zombo <www.mariamzombo@gmail.com>
2023-06-22 12:36:07 +0000

    fixed all the pycodestyle indentation problem

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,13 +7,13 @@ if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/"
     employee = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos",
-        params={'userId': sys.argv[1]}).json()
+                         params={'userId': sys.argv[1]}).json()
 
     tasks = len(todos)
     completed_tasks = [x.get('title') for x in todos
-        if x.get('completed') is True]
+                       if x.get('completed') is True]
 
     print("Employee {} is done with tasks({}/{}):"
-        .format(employee['name'], len(completed_tasks), tasks))
+          .format(employee['name'], len(completed_tasks), tasks))
 
     [print("\t {}".format(each)) for each in completed_tasks]
