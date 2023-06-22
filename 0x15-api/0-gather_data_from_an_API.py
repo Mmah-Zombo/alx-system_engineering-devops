a30c74e40fd2c8176a4c62477e36931e686c46ec
M'mah Zombo <www.mariamzombo@gmail.com>
2023-06-22 12:16:52 +0000

    Fixed the code to meet the pycodestyle standards

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,12 +6,14 @@ import sys
 if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/"
     employee = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos" ,\
+    todos = requests.get(url + "todos",
             params={'userId': sys.argv[1]}).json()
+    
     tasks = len(todos)
-    completed_tasks = [x.get('title') for x in todos if x.get('completed') is True]
+    completed_tasks = [x.get('title') for x in todos 
+                       if x.get('completed') is True]
 
-    print("Employee {} is done with tasks({}/{}):"\
+    print("Employee {} is done with tasks({}/{}):"
         .format(employee['name'], len(completed_tasks), tasks))
 
     [print("\t {}".format(each)) for each in completed_tasks]
