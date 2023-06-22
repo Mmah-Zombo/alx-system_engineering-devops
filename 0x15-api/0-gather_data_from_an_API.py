a8efe20d2ad30ab0052727d09319d79a27d046ce
M'mah Zombo <110876378+Mmah-Zombo@users.noreply.github.com>
2023-06-22 11:33:27 +0000

    used my code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,37 +1,14 @@
 #!/usr/bin/python3
-"returns information about his/her TODO list progress"
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-def get_employee_todo_progress(employee_id):
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    response = requests.get(url)
-    if response.status_code != 200:
-        print(f"Failed to retrieve employee information for ID {employee_id}")
-        return
-
-    employee = response.json()
-    employee_name = employee.get("name")
-
-    url = "https://jsonplaceholder.typicode.com/todos"
-    response = requests.get(url, params={"userId": employee_id})
-    if response.status_code != 200:
-        print(f"Failed to retrieve TODO list for employee ID {employee_id}")
-        return
-
-    todos = response.json()
-    completed_tasks = [todo for todo in todos if todo.get("completed")]
-
-    total_tasks = len(todos)
-    completed_count = len(completed_tasks)
-
-    print(f"Employee {employee_name} is done with tasks ({completed_count}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t{task.get('title')}")
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Please provide the employee ID as a parameter.")
-    else:
-        employee_id = sys.argv[1]
-        get_employee_todo_progress(employee_id)
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
